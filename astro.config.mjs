3923adfb54cb1961db3e20eb8bf71f13cc2ceec8
ROBOT <tkata12@outlook.jp>
2025-05-09 17:03:19 +0900

    add: prettier and eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tutorial.wktk.moe",
-    integrations: [mdx(), sitemap()],
-    vite: {
-        plugins: [tailwindcss()]
-    }
+	site: "https://tutorial.wktk.moe",
+	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
