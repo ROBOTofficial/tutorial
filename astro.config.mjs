afb4a4afcf5b926abd639221d6f320849e4998e5
ROBOT <tkata12@outlook.jp>
2025-05-09 16:56:06 +0900

    add: tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://tutorial.wktk.moe",
-    integrations: [mdx(), sitemap()]
+    integrations: [mdx(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
