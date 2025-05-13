59ab850cea6e570b5a29b9aae6e4735f0d8d3e64
ROBOT <tkata12@outlook.jp>
2025-05-13 16:08:02 +0900

    add: github link in header

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
+export const REPOSITORY = {
+	url: "https://github.com/ROBOTofficial/tutorial"
+};
+
 export const SITE_NAME = "Wktk Tutorial";
 export const SITE_DESCRIPTION =
 	"Wktk Tutorialは初心者向けプログラミング学習サイトです。是非気軽にご覧ください。";
