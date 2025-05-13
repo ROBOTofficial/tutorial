c21f00ac0d655392d57df33d03a2857ff9c0b1d9
ROBOT <tkata12@outlook.jp>
2025-05-13 16:40:45 +0900

    add: pubDate and updatedDate

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,13 +5,17 @@ export const collections = {
 	tutorials: defineCollection({
 		loader: glob({ base: "./src/content/tutorial", pattern: "**/*.{md,mdx}" }),
 		schema: z.object({
-			title: z.string()
+			title: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date()
 		})
 	}),
 	specs: defineCollection({
 		loader: glob({ base: "./src/content/spec", pattern: "**/*.{md,mdx}" }),
 		schema: z.object({
-			title: z.string()
+			title: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date()
 		})
 	})
 };
